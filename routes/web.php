459738f35290ff4dcfdf5c9b63454859ce8d2537
Tristan <your-email@example.com>
2026-02-07 11:07:30 +0700

    ubah di route

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,7 @@ Route::middleware('auth')->group(function () {
 });
 
 use App\Http\Controllers\AdminController;
+
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
