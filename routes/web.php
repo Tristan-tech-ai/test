d4d2e31bea0ac09aded76f080e19081691823d43
Sansv <119500852+Sansv25@users.noreply.github.com>
2026-02-07 12:16:47 +0800

    ganti rute

--- routes/web.php
+++ routes/web.php
@@ -43,5 +43,5 @@ Route::get('/minuman', [AdminController::class, 'index'])->name('minuman.index')
 use App\Http\Controllers\AdminController;
 
 Route::middleware(['auth', 'is_admin'])->group(function () {
-    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/minuman', [AdminController::class, 'index'])->name('minuman.index');
 });
